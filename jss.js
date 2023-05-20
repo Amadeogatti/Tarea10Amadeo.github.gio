9eb8556d21e8b6bd64ff20ccc67f21e569fd67d4
Amadeogatti <130613996+Amadeogatti@users.noreply.github.com>
2023-05-19 22:56:03 -0300

    tarea terminada

---
+++ jss.js
@@ -0,0 +1,13 @@
+var nombre = "Amadeo"; //string
+var apellido = "Gatti"; //string
+var suma = nombre+" "+apellido;
+console.log("suma:", suma);
+
+
+var numberone = 155;
+var numbertwo = 983;
+var Suma = numberone + numbertwo;
+console.log("suma:", Suma)
+var numberthree = 174;
+var Suma2 = Suma + numberthree;
+console.log("suma:", Suma2)
